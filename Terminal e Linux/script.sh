d6af26d6e016ba31294049d36250f2c587ce28ad
Leonardo Gomes <114444222+leonardo-gomes03@users.noreply.github.com>
2023-01-19 08:54:40 -0300

    Fix: Funciona em qualquer linux

--- Terminal e Linux/script.sh
+++ Terminal e Linux/script.sh
@@ -1,15 +1,15 @@
 echo "Digite o nome da pasta a ser criada: "
 read valor
 
-if [ -e "/home/fieldacademy/'$valor'" ]
+if [ -e "~/'$valor'" ]
 then
 echo "O diretório já existe. "
 else
 echo "Diretório não existe, criando um... "
-mkdir /home/fieldacademy/"$valor"
+mkdir ~/"$valor"
 echo "Digite o nome do arquivo .js"
 read scriptNome
-touch /home/fieldacademy/"$valor"/"$scriptNome".js
+touch ~/"$valor"/"$scriptNome".js
 fi
 
 echo "A pasta  '$valor' foi criada" 
